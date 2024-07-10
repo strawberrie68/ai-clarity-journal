c421eb81defd1fce7eb94be3369996829fd6c3cd
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-09 21:40:09 -0700

    create: card component

---
+++ components/common/Card.jsx
@@ -0,0 +1,25 @@
+const Card = ({
+  icon,
+  text,
+  background,
+  secondaryBackground,
+  textBackground,
+}) => {
+  const flexCenter = "flex justify-center items-center";
+  return (
+    <div className={`card h-44 w-40 ${background} rounded-2xl relative`}>
+      <div
+        className={`${textBackground} rounded-2xl absolute bottom-0 h-1/2 w-full z-10`}
+      >
+        <div
+          className={`h-9 w-9 ${secondaryBackground} border-white border-2 rounded-lg ${flexCenter} ml-4 absolute -top-4`}
+        >
+          <img className="h-5" src={icon} />
+        </div>
+        <p className="font-semibold pt-7 pl-4 text-sm line-clamp-2">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
