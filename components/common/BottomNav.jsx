54ca1aa6cc98e53ea6e753d0a819427079c5d6a3
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-11 10:33:37 -0700

    update and fix: BottomNAv and buttons components to make more reusable

--- components/common/BottomNav.jsx
+++ components/common/BottomNav.jsx
@@ -1,41 +1,59 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const BottomNav = () => {
   const flexCenter = "flex justify-center items-center";
+
+  const activeTabStyle = `flex bg-white rounded-full px-4 items-center gap-2 flex-col h-16 w-16 ${flexCenter}`;
+  const inactiveTabStyle = `flex items-center gap-2 flex-col`;
+
+  const activeTextStyle = "text-black text-sm";
+  const inactiveTextStyle = "text-white text-sm";
+
+  const NavLinks = [
+    { id: 1, name: "Inbox", icon: `/calendar-dots`, path: "/home" },
+    { id: 2, name: "Explore", icon: `/explore`, path: "/explore" },
+    { id: 3, name: "Add", icon: `/book`, path: "/add" },
+    { id: 4, name: "Entries", icon: `/books`, path: "/journal/entries" },
+    { id: 5, name: "Review", icon: `/review`, path: "/reviews" },
+  ];
+
+  const pathname = usePathname();
+  const isActive = (path) => path === pathname;
 
   return (
     <div className="bottom-nav sticky mx-auto bottom-4 rounded-full h-20 bg-black  max-w-screen-sm sm:w-10/12 sm:px-12 px-6 content-center">
       <ul className="flex justify-between items-center">
-        <li className="py-2">
-          <div className="flex items-center gap-2 flex-col">
-            <img src="/calendar-dots-white.svg" alt="Inbox" />
-            <p className="text-white text-sm">Inbox</p>
-          </div>
-        </li>
-        <li className="py-2">
-          <div className="flex items-center gap-2 flex-col">
-            <img src="/explore-white.svg" alt="Explore" />
-            <p className="text-white text-sm">Explore</p>
-          </div>
-        </li>
-        <li className="py-2">
-          <div
-            className={`flex bg-white rounded-full px-4 items-center gap-2 flex-col h-16 w-16 ${flexCenter}`}
-          >
-            <img className="" src="/book.svg" alt="Add Journal" />
-            <p className="text-black text-sm">Add</p>
-          </div>
-        </li>
-        <li className="py-2">
-          <div className="flex items-center gap-2 flex-col">
-            <img className="px-2" src="/books-white.svg" alt="Entries" />
-            <p className="text-white text-sm">Entries</p>
-          </div>
-        </li>
-        <li className="py-2">
-          <div className="flex items-center gap-2 flex-col">
-            <img src="/review-white.svg" alt="Review" />
-            <p className="text-white text-sm">Review</p>
-          </div>
-        </li>
+        {NavLinks.map((link) => {
+          return (
+            <li className="py-2" key={link.id}>
+              <Link href={link.path}>
+                <div
+                  className={
+                    isActive(link.path) ? activeTabStyle : inactiveTabStyle
+                  }
+                >
+                  <img
+                    src={
+                      isActive(link.path)
+                        ? link.icon + ".svg"
+                        : link.icon + "-white.svg"
+                    }
+                    alt={link.name}
+                  />
+                  <p
+                    className={
+                      isActive(link.path) ? activeTextStyle : inactiveTextStyle
+                    }
+                  >
+                    {link.name}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- components/common/Button.jsx
+++ components/common/Button.jsx
@@ -1,10 +1,11 @@
-const Button = ({ buttonText, isPrimary, handleClick }) => {
+const Button = ({ buttonText, isPrimary, handleClick, type }) => {
   const buttonStyle = isPrimary ? "bg-black " : "";
   const textStyles = isPrimary ? "text-white" : "text-black";
   return (
     <button
       className={`${buttonStyle} h-9 w-full rounded-xl`}
       onClick={handleClick}
+      type={type}
     >
       <p className={`${textStyles}`}>{buttonText}</p>
     </button>
