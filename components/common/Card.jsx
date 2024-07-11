4a3ca5078ff8ce3360f4ae7a968966a3dbde0171
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-11 13:45:00 -0700

    fix: card css width

--- components/common/Card.jsx
+++ components/common/Card.jsx
@@ -7,14 +7,16 @@ const Card = ({
 }) => {
   const flexCenter = "flex justify-center items-center";
   return (
-    <div className={`card h-44 w-40 ${background} rounded-2xl relative`}>
+    <div
+      className={`card min-h-44 min-w-40 ${background} rounded-2xl relative`}
+    >
       <div
         className={`${textBackground} rounded-2xl absolute bottom-0 h-1/2 w-full z-10`}
       >
         <div
           className={`h-9 w-9 ${secondaryBackground} border-white border-2 rounded-lg ${flexCenter} ml-4 absolute -top-4`}
         >
-          <img className="h-5" src={icon} />
+          <img className="h-5" src={icon} alt={`${icon} icon`} />
         </div>
         <p className="font-semibold pt-7 pl-4 text-sm line-clamp-2">{text}</p>
       </div>
