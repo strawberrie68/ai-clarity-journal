46fb1cab987560b0c90257c9d2d5d0d0102337f9
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-11 11:51:11 -0700

    update: add link to css

--- pages/journal/add.jsx
+++ pages/journal/add.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import "../styles/global.css";
+import "../../styles/global.css";
 import DateTitle from "@/components/common/DateTitle";
 import Header from "@/components/common/Header";
 import Button from "@/components/common/Button";
