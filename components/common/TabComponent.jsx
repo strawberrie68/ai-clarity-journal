709839f7f01e9f794b431dd845cad8f2394bb5d4
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-11 10:34:17 -0700

    create: TabComponent for any tabing pages

---
+++ components/common/TabComponent.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const TabComponent = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(tabs[0].key);
+
+  const handleToggle = (tabKey) => {
+    setActiveTab(tabKey);
+  };
+
+  const activeTabStyle = "bg-black text-white font-medium";
+  const inactiveTabStyle = "bg-zinc-100 text-black";
+  const activeTextStyle = "text-white";
+
+  return (
+    <div>
+      <div className="flex justify-start gap-4 mt-4 items-center">
+        {tabs.map((tab) => (
+          <div
+            key={tab.key}
+            className={`${
+              activeTab === tab.key ? activeTabStyle : inactiveTabStyle
+            } rounded-xl h-9 px-6 flex justify-center items-center cursor-pointer`}
+            onClick={() => handleToggle(tab.key)}
+          >
+            <h3
+              className={`${
+                activeTab === tab.key ? activeTextStyle : ""
+              } text-sm`}
+            >
+              {tab.label}
+            </h3>
+          </div>
+        ))}
+      </div>
+      <div className="content mt-4">
+        {tabs.map((tab) => (
+          <div
+            key={tab.key}
+            style={{ display: activeTab === tab.key ? "block" : "none" }}
+          >
+            {tab.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TabComponent;
