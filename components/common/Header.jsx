878198e6343e2de15634e448e1e86f123e76d7f1
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-11 14:01:02 -0700

    fix: header component added back arrow

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -1,14 +1,16 @@
+import "../../styles/global.css";
+import Link from "next/link";
 const Header = () => {
   const flexCenter = "flex justify-center items-center";
-
   return (
     <header className="flex justify-between">
-      <button className={`bg-zinc-100 h-16 w-16 rounded-full ${flexCenter}`}>
-        <img src="/list.svg" alt="nav-bar" />
-      </button>
-
+      <Link href="/">
+        <button className={`bg-zinc-100 h-16 w-16 rounded-full ${flexCenter}`}>
+          <img src={`/back.svg`} alt="nav-bar" />
+        </button>
+      </Link>
       <div>
-        <img src="/user-profile.png" />
+        <img src="/user-profile.png" alt="user avatar" />
       </div>
     </header>
   );
