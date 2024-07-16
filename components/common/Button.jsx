7bf8ee390b2b19f0080065b97ef6be02fda1e103
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-16 13:09:02 -0700

    update: Button component for hover css

--- components/common/Button.jsx
+++ components/common/Button.jsx
@@ -1,13 +1,15 @@
 const Button = ({ buttonText, isPrimary, handleClick, type }) => {
-  const buttonStyle = isPrimary ? "border border-black" : "";
-  const textStyles = isPrimary ? "text-black font-bold" : "text-gray-500";
+  const buttonStyle = isPrimary
+    ? "border border-black bg-white text-black hover:bg-black hover:text-white"
+    : "text-gray-500 bg-transparent hover:text-black";
+
   return (
     <button
-      className={`${buttonStyle} h-9 w-full rounded-xl`}
+      className={`${buttonStyle} h-9 w-full rounded-xl flex items-center justify-center font-semibold`}
       onClick={handleClick}
       type={type}
     >
-      <p className={`${textStyles}`}>{buttonText}</p>
+      {buttonText}
     </button>
   );
 };
