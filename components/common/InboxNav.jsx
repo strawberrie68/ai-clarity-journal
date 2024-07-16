794e7ce707a8e847d7fd0c1b9fdfc1f01eac07b6
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-15 22:31:09 -0700

    fix: InboxNav component to toggle when active

--- components/common/InboxNav.jsx
+++ components/common/InboxNav.jsx
@@ -1,20 +1,44 @@
 import React, { act, useState } from "react";
 import NavItem from "./NavItem";
+import InboxList from "./InboxList";
 
 const InboxNav = () => {
+  const [activeItem, setActiveItem] = useState("Habits");
+
   return (
-    <nav className="inbox-nav">
-      <ul className="flex justify-between items-center mt-8">
-        <NavItem src="/calendar-blank.svg" text="Habits" />
-        <NavItem src="/calendar-dots.svg" text="Upcoming" />
-        <NavItem src="/arrow-circle-right.svg" text="Next Actions" />
-      </ul>
-      <div className="mt-4 flex w-auto relative">
-        <div className="border-2 w-1/4 border-slate-950"></div>
-        <div className="border h-0 w-1/3 "></div>
-        <div className="border h-0 w-1/3 "></div>
+    <>
+      <nav className="inbox-nav overflow-auto">
+        <ul className="flex justify-between items-center mt-4">
+          <NavItem
+            src="/calendar-blank.svg"
+            text="Habits"
+            isActive={activeItem === "Habits"}
+            onClick={() => setActiveItem("Habits")}
+          />
+          <NavItem
+            src="/calendar-dots.svg"
+            text="Upcoming"
+            isActive={activeItem === "Upcoming"}
+            onClick={() => setActiveItem("Upcoming")}
+          />
+          <NavItem
+            src="/arrow-circle-right.svg"
+            text="Actions"
+            isActive={activeItem === "Actions"}
+            onClick={() => setActiveItem("Actions")}
+          />
+        </ul>
+      </nav>
+      <div className="min-h-24">
+        {activeItem === "Habits" && <InboxList />}
+        {activeItem === "Upcoming" && (
+          <p className="py-4 px-2 text-gray-300">Empty right now.</p>
+        )}
+        {activeItem === "Actions" && (
+          <p className="py-4 px-2 text-gray-300">Empty right now. </p>
+        )}
       </div>
-    </nav>
+    </>
   );
 };
 
