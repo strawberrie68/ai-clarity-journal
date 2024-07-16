7cb002842a5e68c515e415bcda1bc42a157e4dec
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-15 22:31:55 -0700

    chore: NavBar component remove extra div for cleaner code

--- components/common/NavBar.jsx
+++ components/common/NavBar.jsx
@@ -4,14 +4,10 @@ const NavBar = () => {
   const primaryButton = "bg-black text-white rounded-full h-11 px-12";
   const secondaryButton = " text-black h-11 px-12";
   return (
-    <div className="w-full flex mt-6">
-      <div>
-        <div className={`${primaryButton} flexCenter`}>Today</div>
-      </div>
-      <div className="flex">
-        <div className={`${secondaryButton} flexCenter`}>Tasks</div>
-        <div className={`${secondaryButton} flexCenter`}>Habits</div>
-      </div>
+    <div className="w-full flex mt-6 overflow-auto">
+      <div className={`${primaryButton} flexCenter`}>Today</div>
+      <div className={`${secondaryButton} flexCenter`}>Tasks</div>
+      <div className={`${secondaryButton} flexCenter`}>Habits</div>
     </div>
   );
 };
