25dd39cfe45efb316722d02e50e253469823f607
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-16 15:04:14 -0700

    update: icon size in bottomNav component

--- components/common/BottomNav.jsx
+++ components/common/BottomNav.jsx
@@ -39,8 +39,8 @@ const BottomNav = () => {
                         : link.icon + "-white.svg"
                     }
                     alt={link.name}
-                    width={20}
-                    height={20}
+                    width={24}
+                    height={24}
                   />
                   <p
                     className={
