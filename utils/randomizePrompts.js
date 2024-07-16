9301358d4fab5defddfad814c796369ce3035d4f
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-16 13:10:36 -0700

    create: randomizePrompts function to output an array of randomize prompts with a length of 3

---
+++ utils/randomizePrompts.js
@@ -0,0 +1,9 @@
+export const randomizePrompts = (promptsArray) => {
+  const randomPrompts = [];
+  for (let i = 0; i < 3; i++) {
+    const randomPrompt =
+      promptsArray[Math.floor(Math.random() * promptsArray.length)];
+    randomPrompts.push(randomPrompt);
+  }
+  return randomPrompts;
+};
