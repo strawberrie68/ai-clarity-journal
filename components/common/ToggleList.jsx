abe970e89299b04bcaea26353efbe341e17805ec
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-15 22:36:24 -0700

    create: ToggleList component

---
+++ components/common/ToggleList.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from "react";
+import { useRouter } from "next/navigation";
+import NavItem from "./NavItem";
+import "../../styles/global.css";
+
+const ToggleList = () => {
+  const [activeItem, setActiveItem] = useState("All");
+  const { push } = useRouter();
+  const { setJournalPrompts } = useContext(JournalContext);
+
+  const handleCardClick = (prompts) => {
+    setJournalPrompts(prompts);
+    push("/journal/add");
+    console.log("prompts on toggleList", prompts);
+  };
+  return (
+    <>
+      <nav className="mt-4">
+        <ul className="flex justify-start">
+          <NavItem
+            src="/calendar-blank.svg"
+            text="All"
+            isActive={activeItem === "All"}
+            onClick={() => setActiveItem("All")}
+          />
+          <NavItem
+            src="/calendar-dots.svg"
+            text="Life"
+            isActive={activeItem === "Life"}
+            onClick={() => setActiveItem("Life")}
+          />
+          <NavItem
+            src="/arrow-circle-right.svg"
+            text="Productivity"
+            isActive={activeItem === "Productivity"}
+            onClick={() => setActiveItem("Productivity")}
+          />
+        </ul>
+      </nav>
+      <div className="min-h-24">
+        {activeItem === "All" && (
+          <section className="flex flex-col gap-2 mt-4">
+            <div
+              className="border rounded-lg px-2 py-3"
+            >
+              <p>❤️ Find your passion</p>
+            </div>
+            <div
+              className="border rounded-lg px-2 py-3"
+            >
+              <p>✨ Be Productive</p>
+            </div>
+          </section>
+        )}
+        {activeItem === "Life" && (
+          <p className="py-4 px-2 text-gray-300">Life</p>
+        )}
+        {activeItem === "Productivity" && (
+          <p className="py-4 px-2 text-gray-300">Productivity</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ToggleList;
