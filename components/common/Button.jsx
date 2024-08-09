8e9850f82154c772a2a6592bd956c7a008a7527d
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-08-08 17:22:29 -0700

    add: new button componet

--- components/common/Button.jsx
+++ components/common/Button.jsx
@@ -2,7 +2,6 @@ const Button = ({ buttonText, isPrimary, handleClick, type, disabled }) => {
   const buttonStyle = isPrimary
     ? "border border-black bg-white text-black hover:bg-black hover:text-white"
     : "text-gray-500 bg-white hover:text-black";
-
   return (
     <button
       className={`${buttonStyle} h-9 w-full rounded-xl flex items-center justify-center font-semibold`}
---
+++ components/common/ButtonCopy.jsx
@@ -0,0 +1,26 @@
+const ButtonCopy = ({
+  buttonText,
+  buttonType,
+  handleClick,
+  type,
+  disabled,
+}) => {
+  const buttonStyle2 = {
+    primary: "rounded-3xl bg-black text-white button-shadow font-semibold",
+    secondary:
+      "border text-gray-700 bg-white border-gray-300 hover:bg-lt-gray rounded-3xl",
+    "primary-2": "bg-lt-gray text-black font-medium",
+  };
+  return (
+    <button
+      className={`${buttonStyle2[buttonType]} h-14 w-full rounded-3xl flex items-center justify-center`}
+      onClick={handleClick}
+      type={type}
+      disabled={disabled}
+    >
+      {buttonText}
+    </button>
+  );
+};
+
+export default ButtonCopy;
