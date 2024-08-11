6f66c12a2bcd81ed49a74ee340f3b00d1c11bffe
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-08-10 22:07:46 -0700

    update: journal /journalId jsx page to tsx

--- pages/journal/[journalId]/index.tsx
+++ pages/journal/[journalId]/index.tsx
@@ -1,21 +1,49 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../AuthContext.js";
-
-import Header from "../../../components/common/Header";
+import axios from 'axios';
+import Header from "../../../components/common/Header.jsx";
 import TabComponent from "@/components/common/TabComponent";
 import BottomNav from "@/components/common/BottomNav";
 import { formatDate, formattedHaiku } from "@/utils/formatUtils";
-
 import "../../../styles/global.css";
 
-const PastEntry = () => {
-  const [journal, setJournal] = useState({});
+interface Journal {
+  keyInsight?: string;
+  quote?: string;
+  aiSummary?: string;
+  color?: string;
+  conversationSummary?: string;
+  emoji?: string;
+  haiku?: string;
+  highlight?: string;
+  mood?: string;
+  sentiment?: string;
+  title?: string;
+  user: string;
+  entries: Entry[];
+  date: string;
+}
+
+interface Entry {
+  aiResponse: string;
+  content: string;
+  _id: string;
+}
+
+interface Tab {
+  key: string;
+  label: string;
+  content: JSX.Element;
+}
+
+const PastEntry: React.FC = () => {
+  const [journal, setJournal] = useState<Journal | null>(null);
   const router = useRouter();
-  const { journalId } = router.query;
+  const { journalId } = router.query as { journalId: string }; // Adding type assertion
   const { userId } = useAuth();
 
-  function formatJournalEntry(entry) {
+  function formatJournalEntry(entry: string): JSX.Element[] {
     entry = entry.replace(/:/g, ":<br/>");
     entry = entry.replace(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g, "<br/><br/>$1");
     const lines = entry.split("\n");
@@ -27,16 +55,16 @@ const PastEntry = () => {
 
   useEffect(() => {
     const fetchJournal = async () => {
-      const response = await fetch(
+      const response = await axios.get(
         `/api/users/${userId}/journal/entries/${journalId}`
       );
-      const data = await response.json();
+      const data: Journal[] = response.data;
       setJournal(data[0]);
     };
     fetchJournal();
-  }, [journalId]);
+  }, [journalId, userId]);
 
-  const tabs = [
+  const tabs: Tab[] = [
     {
       key: "summary",
       label: "Summary",
@@ -47,56 +75,46 @@ const PastEntry = () => {
           </h2>
           <section className="w-full min-h-16 px-4 py-8 flex flex-col gap-4 rounded-lg bg-gradient-to-r from-lime-100 to-teal-100">
             <h2 className="text-lg font-semibold max-w-prose">
-              {`${journal && journal.emoji} ${journal && journal.title}`}
+              {`${journal?.emoji || ""} ${journal?.title || ""}`}
             </h2>
-            <p className="max-w-prose mt-2">{journal && journal.aiSummary}</p>
+            <p className="max-w-prose mt-2">{journal?.aiSummary}</p>
           </section>
           <section className="mt-8 flex flex-col gap-4">
             <h3 className="text-md font-semibold text-gray-500">Highlights</h3>
 
-            <div
-              className={`border rounded-xl px-4 py-4 flex flex-col justify-center bg-zinc-100`}
-            >
+            <div className="border rounded-xl px-4 py-4 flex flex-col justify-center bg-zinc-100">
               <h3 className="font-semibold text-sm text-neutral-400">
                 ⭐️ Key Insight:
               </h3>
               <p className="mt-2 max-w-prose">
-                {journal && journal.keyInsight}
+                {journal?.keyInsight}
               </p>
             </div>
-            <div
-              className={`border rounded-xl px-4 py-2 flex flex-col justify-center`}
-            >
+            <div className="border rounded-xl px-4 py-2 flex flex-col justify-center">
               <h3 className="font-semibold text-sm text-neutral-300">
                 😶 Mood:
               </h3>
-              <div className="mt-2">{journal && journal.mood}</div>
+              <div className="mt-2">{journal?.mood}</div>
             </div>
 
-            <div
-              className={`border rounded-xl px-4 py-2 flex flex-col justify-center`}
-            >
+            <div className="border rounded-xl px-4 py-2 flex flex-col justify-center">
               <h3 className="font-semibold text-sm text-neutral-300">
                 💭 Quote:
               </h3>
-              <p className="mt-2 max-w-prose">{journal && journal.quote}</p>
+              <p className="mt-2 max-w-prose">{journal?.quote}</p>
             </div>
-            <div
-              className={`border rounded-xl px-4 py-2 flex flex-col justify-center`}
-            >
+            <div className="border rounded-xl px-4 py-2 flex flex-col justify-center">
               <h3 className="font-semibold text-sm text-neutral-300">
                 🍦 Highlight:
               </h3>
-              <p className="mt-2 max-w-prose">{journal && journal.highlight}</p>
+              <p className="mt-2 max-w-prose">{journal?.highlight}</p>
             </div>
-            <div
-              className={`border rounded-xl px-4 py-2 flex flex-col justify-center`}
-            >
+            <div className="border rounded-xl px-4 py-2 flex flex-col justify-center">
               <h3 className="font-semibold text-sm text-neutral-300">
                 🎨 Haiku:
               </h3>
               <div className="mt-2">
-                {journal && formattedHaiku(journal.haiku)}
+                {journal && formattedHaiku(journal.haiku || "")}
               </div>
             </div>
           </section>
@@ -111,19 +129,18 @@ const PastEntry = () => {
           <h2 className="text-md mt-10 font-bold mb-2 text-gray-500">
             {journal && formatDate(journal.date)}
           </h2>
-          <div className="">
+          <div>
             {journal &&
-              journal.entries?.map((entry) => (
+              journal.entries.map((entry) => (
                 <div key={entry._id}>
                   <section className="w-full min-h-16 px-4 py-8 flex flex-col gap-4 rounded-lg bg-gradient-to-r from-lime-100 to-teal-100">
                     <h2 className="text-lg font-semibold max-w-prose pb-4">
-                      {`${journal && journal.emoji} 
-              ${journal && journal.title}`}
+                      {`${journal.emoji} ${journal.title}`}
                     </h2>
                     {formatJournalEntry(entry.content)}
                   </section>
                   <section className="w-full min-h-16 mt-4 px-4 py-4 flex flex-col gap-4 rounded-lg border">
-                    <p className="max-w-prose ">{entry.aiResponse}</p>
+                    <p className="max-w-prose">{entry.aiResponse}</p>
                   </section>
                 </div>
               ))}
