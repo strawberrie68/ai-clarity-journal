7b64a23d0cfb0431a2bddc4adcbceb5971c8eaf1
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-10-29 18:58:44 -0700

    chore: fixes and update for typescript

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import "../../styles/global.css";
 
-const Header = () => {
+interface HeaderProps {
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ handleClick }) => {
   return (
     <header className="flex justify-between">
       <Link href="/">
-        <button className="bg-zinc-100 h-11 w-11 rounded-full flexCenter">
+        <button className="bg-zinc-100 h-11 w-11 rounded-full flexCenter" onClick={handleClick}>
           <Image src={`/back.svg`} alt="nav-bar" width={18} height={18} />
         </button>
       </Link>
---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = [
+    "#E0F7FA", // Soft cyan
+    "#F8BBD0", // Muted pink
+    "#C8E6C9", // Light mint green
+    "#FFF9C4", // Soft yellow
+    "#D1C4E9", // Light lavender
+    "#FFECB3", // Muted peach
+    "#FFE0B2", // Pale orange
+    "#FFCCBC", // Soft coral
+    "#D7CCC8"  // Muted beige
+];
